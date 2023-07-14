559a2d78a5146093bb041fee11a22db052bbd8ca
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-13 18:51:17 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@
 */
 
 int main(void)
+{	
 	printf("Size of a char: %d byte(s)", sizeof(char));
 	printf("Size of an int: %d byte(s)", sizeof(int));
 	printf("Size of a long int: %d byte(s)", sizeof(longint));
