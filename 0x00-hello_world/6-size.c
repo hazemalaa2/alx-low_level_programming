317ed66037eef0b05123f69e38d3706501c61e48
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-13 18:50:33 -0700

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ *main - entry point
+ *
+ *Description: A C program that prints Data sizes
+ *
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(longint));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}				
