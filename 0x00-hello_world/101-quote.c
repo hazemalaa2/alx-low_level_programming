fb28f41ed3c970b0afedf0ac83beb4c46ec42755
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-13 19:15:50 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+
+/**
+ * main - entry point
+ *
+ * Description: print a code using write_funtion
+ *	write(int fd,const void)
+ *
+ * Return: 1 (Not Success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write[1, quo, 59];
+}
