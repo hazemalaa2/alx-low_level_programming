8aec31fec50af01467da36e747ac3f51afc6b160
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-14 19:34:17 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ *main - entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= a)
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n'):
+	
+	return (0);
+}
+
