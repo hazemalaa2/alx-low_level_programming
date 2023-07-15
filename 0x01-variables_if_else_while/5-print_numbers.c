837947b29d1bc068f4cf1bdc15cf44dcddb4b760
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-14 19:06:06 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ *main - entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *Return: Always 0 (Success)
+*/
+
+
+int main(void)
+	int digit = 0;
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	
+	return (0);
+}
