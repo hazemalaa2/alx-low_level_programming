d06d83046cf5d857c3246c603814cc3a287e29dd
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-18 19:22:21 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,8 +14,11 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('.');
-		_putchar(' ');
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar('.');
+			_putchar(' ');
 
 		prod = num * mult;
 
@@ -27,4 +30,5 @@ void times_table(void)
 		_putchar((prod % 10) + 48);
 	}
 	_putchar('\n');
+	}
 }
