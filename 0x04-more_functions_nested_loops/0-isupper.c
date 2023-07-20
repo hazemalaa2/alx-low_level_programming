753889270c35bb13ce64324c4bd783ddae1659f5
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-20 08:09:05 -0700

    a

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - entry point
+ *
+ * @c: function 
+ *
+ *Return: Always 1 if upper else 0 (Success)
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (100);
+	else
+		return (0);
+}
