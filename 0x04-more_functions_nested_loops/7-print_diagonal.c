0c4d66a3b50ec8023a4d45a421f12be01a2cbb26
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-21 06:49:25 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,8 +2,24 @@
 
 /**
  * print_diagonal - entry point
- *
+ * @n: sad1
  *Return: Always 0 (Success)
 */
 
+void print_diagonal(int n)
+{
+	int postn, space;
 
+	if (n <=0)
+		_putchat('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
