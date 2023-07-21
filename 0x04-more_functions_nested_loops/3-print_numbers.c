d89fbc7b0a1eeead1a6731055a531c82978ec79e
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-21 01:58:34 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,13 +10,11 @@
 
 void print_numbers(void)
 {
-	int num = 0;
+	char c;
 
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9)
-	_putchar ('\n');
-
-	return (0);
+	for (c = '0'; c <= '9'; c++;)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
