b11ad75b48e97af9cebdf3f985f217875d5285b6
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-24 18:21:16 -0700

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - entry point
+ *
+ * @a: function
+ * @n: ree
+ *Return: Always 0 (Success)
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf('\n');
+}
