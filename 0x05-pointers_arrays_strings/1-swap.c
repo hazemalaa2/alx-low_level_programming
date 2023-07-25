d55e53b0030c08d68aaa6d0d862cba2492f985d3
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-24 17:31:59 -0700

    a

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - entry point
+ *
+ * @a: function
+ * @b: sa
+ *
+ *Return: Always 0 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
