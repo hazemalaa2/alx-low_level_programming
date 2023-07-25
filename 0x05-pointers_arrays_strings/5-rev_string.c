db1cea00ef9ad9907981e4050a60ab27cef3ab8d
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-24 18:00:50 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ *
+ * @s: function
+ *
+ *Return: Always 0 (Success)
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != 0; ++l)
+		;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[l];
+		s[i] = s[l -1 - i];
+		s[l -1 - i]; = temp;
+	}
+}
