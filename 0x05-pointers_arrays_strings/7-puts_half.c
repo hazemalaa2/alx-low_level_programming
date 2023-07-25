885cca5fa64fcfb1e10b19253c44e41379b6fe05
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-24 18:15:06 -0700

    a

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - entry point
+ *
+ * @str: function
+ *
+ *Return: Always 0 (Success)
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
