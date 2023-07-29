4c5d963b30240c560c130360ca8ae57128cfc67f
hazemalaa2 <hazemzoka2003@yahoo.com>
2023-07-28 17:54:29 -0700

    a

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - entry point
+ * @a: input value
+ * @n: number of elements
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+	int j;
+
+	for (i = 0; j = (n - 1); i < j; i++; j--;)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
